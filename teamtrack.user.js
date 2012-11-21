6020049f23ff621cfec102e1f527df37a70df42a
Ryan Mechelke <rfmechelke@gmail.com>
2012-11-21 10:08:19 -0600

    Initial version of tooltip for ticket descriptions

--- teamtrack.user.js
+++ teamtrack.user.js
@@ -6,29 +6,43 @@
 // ==/UserScript==
 
 function injectScripts(callback) {
-    console.log("injecting jquery");
     var jquery = document.createElement('script');
     jquery.type = 'text/javascript';
-    jquery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js';
+    jquery.src = 'http://code.jquery.com/jquery-1.8.3.js';
     jquery.addEventListener('load', function () {
-        var myScript = document.createElement("script");
-        myScript.textContent = "(" + callback.toString() + ")();";
-        document.body.appendChild(myScript);
+        var jqueryui = document.createElement('script');
+        jqueryui.type = 'text/javascript';
+        jqueryui.src = 'http://code.jquery.com/ui/1.9.1/jquery-ui.js';
+        jqueryui.addEventListener('load', function () {
+            var myScript = document.createElement("script");
+            myScript.textContent = "(" + callback.toString() + ")();";
+            document.body.appendChild(myScript);
+        });
+        document.body.appendChild(jqueryui);
     });
     document.body.appendChild(jquery);
 }
 
 function main() {
-    console.log("TeamTrack script running");
 
     // insert stylesheet
     $(document.body).append($("<style>", {
-        text: ".listrow-header { background-color: grey; font-weight: bold; }"
+        text:
+            ".listrow-header { " +
+                "background-color: grey;" +
+                "font-weight: bold;" +
+            "}" +
+            ".tooltip { " +
+                "background-color: #ddd;" +
+                "border: 1px solid black;" +
+                "border-radius: 5px;" +
+                "font-size: 0.75em;" +
+                "max-width: 75%;" +
+            "}"
     }));
 
     // insert header row
     $header = $("<tr>");
-    console.log($(".listrow0"));
     $(".listrow0").first().before($header);
     $("<th>", {
         colspan: 3,
@@ -50,7 +64,8 @@ function main() {
     // do ajax queries and get data for each item in the list
     $(".listrow0").each(function () {
         var url = "tmtrack.dll?IssuePage&TableId=1&RecordId={id}&Template=viewbody";
-        var href = $("a", this).attr("href");
+        var $anchor = $("a", this);
+        var href = $anchor.attr("href");
         var split = href.split(/\?|&/);
         var recordId;
 
@@ -79,6 +94,31 @@ function main() {
         // do the actual ajax call
         if (recordId) {
             url = url.replace("{id}", recordId);
+
+            // hook up tooltip
+            $anchor.tooltip({
+                delay: 2000,
+                items: "a",
+                content: function () {
+                    var result;
+
+                    $.ajax({
+                        url: url,
+                        dataType: "html",
+                        async: false,
+                        success: function (data) {
+                            $(".ttfieldname", data).each(function () {
+                                if (/Description/.test($(this).text())) {
+                                    result = $(this).next().html();
+                                    return false;
+                                }
+                            });
+                        }
+                    });
+                    return "<div class='tooltip'>" + result + "</div>";
+                }
+            });
+
             $.ajax({
                 url: url,
                 dataType: "html",
